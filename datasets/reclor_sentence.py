* Add config for adam betas, and fix the bug of clipping gradient norm.

datasets/reclor_sentence.py
@@ -1,36 +1,20 @@
 import json
 import os
-from nltk import sent_tokenize
-from multiprocessing import Pool
+from collections import Counter
 from functools import partial
-from tqdm import tqdm
+from multiprocessing import Pool
+from typing import Dict, List
 
 import torch
-from torch import Tensor
-from typing import Tuple, Callable, Union, Dict, List
-from torch.utils.data import TensorDataset, Dataset
-from transformers import PreTrainedTokenizer, RobertaTokenizer
-from transformers.tokenization_utils_base import PaddingStrategy, TruncationStrategy, TensorType
-from collections import Counter
+from nltk import sent_tokenize
+from tqdm import tqdm
+from transformers import PreTrainedTokenizer
+from transformers.tokenization_utils_base import PaddingStrategy, TruncationStrategy
 
+from datasets.data_utils import get_sep_tokens, is_bpe
 from general_util.logger import get_child_logger
 
 logger = get_child_logger("ReClor.Sentence")
-
-
-def is_bpe(_tokenizer: PreTrainedTokenizer):
-    return _tokenizer.__class__.__name__ in [
-        "RobertaTokenizer",
-        "LongformerTokenizer",
-        "BartTokenizer",
-        "RobertaTokenizerFast",
-        "LongformerTokenizerFast",
-        "BartTokenizerFast",
-    ]
-
-
-def get_sep_tokens(_tokenizer: PreTrainedTokenizer):
-    return [_tokenizer.sep_token] * (_tokenizer.max_len_single_sentence - _tokenizer.max_len_sentences_pair)
 
 
 def read_examples(file_path: str):

reclor_trainer_base.py
@@ -153,7 +153,7 @@ def train(cfg, train_dataset, features, model, tokenizer, continue_from_global_s
             {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)],
              'weight_decay': 0.0}
         ]
-        optimizer = AdamW(optimizer_grouped_parameters, lr=cfg.learning_rate, eps=cfg.adam_epsilon)
+        optimizer = AdamW(optimizer_grouped_parameters, lr=cfg.learning_rate, eps=cfg.adam_epsilon, betas=eval(cfg.adam_betas))
         scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=num_warmup_steps, num_training_steps=t_total)
 
     if cfg.fp16:
@@ -186,6 +186,8 @@ def train(cfg, train_dataset, features, model, tokenizer, continue_from_global_s
         ]
         optimizer = AdamW(optimizer_grouped_parameters, lr=cfg.learning_rate, eps=cfg.adam_epsilon)
         scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=num_warmup_steps, num_training_steps=t_total)
+
+    logger.info(optimizer)
 
     # Train!
     logger.info("***** Running training *****")
@@ -236,12 +238,13 @@ def train(cfg, train_dataset, features, model, tokenizer, continue_from_global_s
                 if cfg.fp16:
                     scaler.unscale_(optimizer)
 
-                if hasattr(optimizer, "clip_grad_norm"):
-                    optimizer.clip_grad_norm(cfg.max_grad_norm)
-                elif hasattr(model, "clip_grad_norm_"):
-                    model.clip_grad_norm_(cfg.max_grad_norm)
-                else:
-                    torch.nn.utils.clip_grad_norm_(model.parameters(), cfg.max_grad_norm)
+                if cfg.max_grad_norm:
+                    if hasattr(optimizer, "clip_grad_norm"):
+                        optimizer.clip_grad_norm(cfg.max_grad_norm)
+                    elif hasattr(model, "clip_grad_norm_"):
+                        model.clip_grad_norm_(cfg.max_grad_norm)
+                    else:
+                        torch.nn.utils.clip_grad_norm_(model.parameters(), cfg.max_grad_norm)
 
                 if cfg.fp16:
                     scaler.step(optimizer)