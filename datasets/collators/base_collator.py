ec86ac65a7374a5524d72007bcc9044f48946920
sparkjiao <jiaofangkai@hotmail.com>
2021-07-15 21:49:10 +0800

    * Add collator and model for pre-training to support the MLM multi-task pre-training.

--- datasets/collators/base_collator.py
+++ datasets/collators/base_collator.py
@@ -1,9 +1,16 @@
-from torch import Tensor
+from typing import Tuple, List, Dict, Optional
+
 import torch
-from typing import Tuple, List, Dict
+from torch import Tensor
+from transformers import AutoTokenizer, RobertaTokenizer
 
 
 class BaseDictCollator:
+    def __init__(self, add_mlm_labels: bool = False, mlm_probability: float = 0.15, tokenizer: str = 'bert-base-uncased'):
+        self.add_mlm_labels = add_mlm_labels
+        self.mlm_probability = mlm_probability
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer)
+
     def __call__(self, batch: List[Tuple[Tensor, ...]]) -> Dict[str, Tensor]:
         if len(batch[0]) == 4:
             input_ids, attention_mask, token_type_ids, labels = list(zip(*batch))
@@ -25,4 +32,46 @@ class BaseDictCollator:
         if token_type_ids is not None:
             outputs["token_type_ids"] = torch.stack(token_type_ids, dim=0)
 
+        if self.add_mlm_labels:
+            mlm_input_ids = input_ids[:, 0].clone()
+            mlm_input_ids, mlm_labels = self.mask_tokens(mlm_input_ids)
+
+            outputs["mlm_input_ids"] = mlm_input_ids
+            outputs["mlm_labels"] = mlm_labels
+
         return outputs
+
+    def mask_tokens(
+        self, inputs: torch.Tensor, special_tokens_mask: Optional[torch.Tensor] = None
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Prepare masked tokens inputs/labels for masked language modeling: 80% MASK, 10% random, 10% original.
+        """
+        labels = inputs.clone()
+        # We sample a few tokens in each sequence for MLM training (with probability `self.mlm_probability`)
+        probability_matrix = torch.full(labels.shape, self.mlm_probability)
+        if special_tokens_mask is None:
+            special_tokens_mask = [
+                self.tokenizer.get_special_tokens_mask(val, already_has_special_tokens=True) for val in labels.tolist()
+            ]
+            special_tokens_mask = torch.tensor(special_tokens_mask, dtype=torch.bool)
+            # Remove padding.
+            special_tokens_mask = special_tokens_mask | (labels == self.tokenizer.pad_token_id)
+        else:
+            special_tokens_mask = special_tokens_mask.bool()
+
+        probability_matrix.masked_fill_(special_tokens_mask, value=0.0)
+        masked_indices = torch.bernoulli(probability_matrix).bool()
+        labels[~masked_indices] = -1  # We only compute loss on masked tokens
+
+        # 80% of the time, we replace masked input tokens with tokenizer.mask_token ([MASK])
+        indices_replaced = torch.bernoulli(torch.full(labels.shape, 0.8)).bool() & masked_indices
+        inputs[indices_replaced] = self.tokenizer.convert_tokens_to_ids(self.tokenizer.mask_token)
+
+        # 10% of the time, we replace masked input tokens with random word
+        indices_random = torch.bernoulli(torch.full(labels.shape, 0.5)).bool() & masked_indices & ~indices_replaced
+        random_words = torch.randint(len(self.tokenizer), labels.shape, dtype=torch.long)
+        inputs[indices_random] = random_words[indices_random]
+
+        # The rest of the time (10% of the time) we keep the masked input tokens unchanged
+        return inputs, labels
--- models/roberta_baseline.py
+++ models/roberta_baseline.py
@@ -15,12 +15,15 @@ logger = get_child_logger("RoBERTa")
 class RobertaForMultipleChoice(RobertaPreTrainedModel, LogMixin, ABC):
     _keys_to_ignore_on_load_missing = [r"position_ids"]
 
-    def __init__(self, config: RobertaConfig):
+    def __init__(self, config: RobertaConfig, re_init_cls: bool = False):
         super().__init__(config)
 
         self.roberta = RobertaModel(config)
         self.lm_head = RobertaLMHead(config)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.re_init_cls = re_init_cls
+        if self.re_init_cls:
+            self.classifier_i = nn.Linear(config.hidden_size, 1)
         self.classifier = nn.Linear(config.hidden_size, 1)
 
         self.init_weights()
@@ -28,7 +31,7 @@ class RobertaForMultipleChoice(RobertaPreTrainedModel, LogMixin, ABC):
         self.init_metric("loss", "acc")
 
     @staticmethod
-    def fold_tensor(x):
+    def fold_tensor(x: Tensor):
         if x is None:
             return x
         return x.reshape(-1, x.size(-1))
@@ -71,7 +74,10 @@ class RobertaForMultipleChoice(RobertaPreTrainedModel, LogMixin, ABC):
         pooled_output = outputs[1]
 
         pooled_output = self.dropout(pooled_output)
-        logits = self.classifier(pooled_output)
+        if self.re_init_cls:
+            logits = self.classifier_i(pooled_output)
+        else:
+            logits = self.classifier(pooled_output)
         reshaped_logits = logits.view(-1, num_choices)
 
         choice_mask = (attention_mask.sum(dim=-1) == 0).reshape(-1, num_choices)
@@ -91,6 +97,114 @@ class RobertaForMultipleChoice(RobertaPreTrainedModel, LogMixin, ABC):
                 acc, true_label_num = layers.get_accuracy(reshaped_logits, labels)
                 self.eval_metrics.update("acc", val=acc, n=true_label_num)
                 self.eval_metrics.update("loss", val=loss.item(), n=true_label_num)
+
+        if not return_dict:
+            output = (reshaped_logits,) + outputs[2:]
+            return ((loss,) + output) if loss is not None else output
+
+        return MultipleChoiceModelOutput(
+            loss=loss,
+            logits=reshaped_logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
+
+
+class RobertaForMultipleChoiceForPreTrain(RobertaPreTrainedModel, LogMixin, ABC):
+    _keys_to_ignore_on_load_missing = [r"position_ids"]
+
+    def __init__(self, config: RobertaConfig, mlp_hidden_size: int = 768):
+        super().__init__(config)
+
+        self.roberta = RobertaModel(config)
+        self.lm_head = RobertaLMHead(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.vocab_size = config.vocab_size
+
+        self.pooler = nn.Sequential(
+            nn.Linear(config.hidden_size, mlp_hidden_size),
+            nn.Tanh()
+        )
+        self.cls = nn.Linear(mlp_hidden_size, 1)
+
+        self.init_weights()
+
+        self.init_metric("loss", "acc", "mlm_loss", "mlm_acc")
+
+    @staticmethod
+    def fold_tensor(x: Tensor):
+        if x is None:
+            return x
+        return x.reshape(-1, x.size(-1))
+
+    def forward(
+            self,
+            input_ids: Tensor,
+            attention_mask: Tensor = None,
+            token_type_ids: Tensor = None,
+            labels: Tensor = None,
+            mlm_input_ids: Tensor = None,
+            mlm_labels: Tensor = None,
+            output_attentions=None,
+            output_hidden_states=None,
+            return_dict=None,
+    ):
+        r"""
+        labels (:obj:`torch.LongTensor` of shape :obj:`(batch_size,)`, `optional`):
+            Labels for computing the multiple choice classification loss. Indices should be in ``[0, ...,
+            num_choices-1]`` where :obj:`num_choices` is the size of the second dimension of the input tensors. (See
+            :obj:`input_ids` above)
+        """
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+        num_choices = input_ids.shape[1]
+
+        input_ids = self.fold_tensor(input_ids)
+        attention_mask = self.fold_tensor(attention_mask)
+        token_type_ids = self.fold_tensor(token_type_ids)
+
+        outputs = self.roberta(
+            input_ids,
+            token_type_ids=token_type_ids,
+            attention_mask=attention_mask,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+        pooled_output = outputs[0][:, 0]
+
+        logits = self.cls(self.dropout(self.pooler(pooled_output)))
+        reshaped_logits = logits.view(-1, num_choices)
+
+        choice_mask = (attention_mask.sum(dim=-1) == 0).reshape(-1, num_choices)
+        reshaped_logits = reshaped_logits + choice_mask * -10000.0
+
+        loss = None
+        if labels is not None:
+            loss_fct = CrossEntropyLoss(ignore_index=-1)
+            loss = loss_fct(reshaped_logits, labels)
+
+            if mlm_labels is not None:
+                mlm_attention_mask = attention_mask.reshape(reshaped_logits.size(0), num_choices, -1)[:, 0]
+
+                mlm_outputs = self.roberta(
+                    mlm_input_ids,
+                    attention_mask=mlm_attention_mask,
+                    return_dict=return_dict
+                )
+
+                mlm_scores = self.lm_head(mlm_outputs[0])
+                mlm_loss = loss_fct(mlm_scores.reshape(-1, self.vocab_size), mlm_labels.reshape(-1))
+                loss += mlm_loss
+
+            if not self.training:
+                acc, true_label_num = layers.get_accuracy(reshaped_logits, labels)
+                self.eval_metrics.update("acc", val=acc, n=true_label_num)
+                self.eval_metrics.update("loss", val=loss.item(), n=true_label_num)
+
+                if mlm_labels is not None:
+                    acc, true_label_num = layers.get_accuracy(mlm_scores, mlm_labels)
+                    self.eval_metrics.update("mlm_acc", val=acc, n=true_label_num)
+                    self.eval_metrics.update("mlm_loss", val=mlm_loss.item(), n=true_label_num)
 
         if not return_dict:
             output = (reshaped_logits,) + outputs[2:]
